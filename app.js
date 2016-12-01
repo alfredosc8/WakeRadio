f811b72e05433c9dc1fc15b34f873c2b5e5e4ae2
Nathan Soufflet <nathsou.fr@gmail.com>
2016-12-01 21:12:52 +0100

    pause now working

--- app.js
+++ app.js
@@ -14,33 +14,35 @@ app.get('/', (req, res) => {
 
 http.listen(3000, () => {
   console.log('listening on port 3000');
-  exec('mplayer http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio3_mf_p', (err, out) => {
+  exec('rem control_fifo');
+  exec('mkfifo control_fifo');
+  exec('mplayer -slave -input file=./control_fifo http://stream.srg-ssr.ch/m/rsc_fr/mp3_128', (err, out) => {
 	  if (err) console.log(err);
 	  if (out) console.log(out);
   });
 });
 
-let getVolume = (handler) => {
-	exec('mpc volume', (err, out) => {
-		
+function mplayer(cmd) {
+	exec('echo ' + cmd + ' > control_fifo', (err, out) => {return {err: err, out: out};});
+}
+
+var getVolume = (handler) => {
+	mplayer('get_property volume', (err, out) => {
+		handler(parseInt(out.split('=')[1]));
 	});
 };
 
-let getCurrentlyPlaying = (handler) => {
+var getCurrentlyPlaying = (handler) => {
 	exec('mpc current', (err, out) => {
 		handler(out);
 	});
 
-let setVolumeDown = (handler) => {
+var setVolumeDown = (handler) => {
 		exec('/');
 };
 
-let setVolumeUp = (handler) => {
+var setVolumeUp = (handler) => {
 	exec('*');
-};
-
-let pause = (handler) => {
-	exec('p');
 };
 
 };
@@ -65,12 +67,8 @@ io.on('connection', (socket) => {
 	});	
 
 	socket.on('pause', () => {
-		pause();
-		console.log('pause');
+		 mplayer('pause');
+		 console.log('pause');
 	});
 
-	socket.on('play', () => {
-		pause();
-		console.log('play');
-	});
 });
