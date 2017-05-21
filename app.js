4ddeeda5b1e4d9e2675a187b19489a7fcfc0c5c6
Nathan Soufflet <nathsou.fr@gmail.com>
2017-05-21 22:51:40 +0200

    Small improvements

--- app.js
+++ app.js
@@ -8,24 +8,41 @@ const express = require('express'),
 
 app.use(express.static(__dirname + '/public'));
 
-var player = new Mplayer('http://stream.srg-ssr.ch/m/rsc_fr/mp3_128');
-player.play();
+var url = 'http://stream.srg-ssr.ch/m/rsc_fr/mp3_128';
+
+var player = new Mplayer(url);
+
+var alarm_timestamps = [];
+
+//check regularly if an alarm should ring
+
+setInterval(() => {
+	if (alarm_timestamps.length !== 0 && alarm_timestamps[0] < new Date().getTime()) {
+		player.play();
+		alarm_timestamps.splice(0, 1);
+	}
+}, 1000);
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, () => {
-  console.log('listening on port 3000');
-});
+	console.log('listening on port 3000');
+	player.play({ volume: 30 });
 
+	player.rl.on('line', (input) => {
+		console.log(`Received: ${input}`);
+	});
+
+});
 
 io.on('connection', (socket) => {
 	console.log('a user connected');
 
 	socket.on('set_volume', (new_vol) => {
 		console.log(new_vol);
-		player.setVolume(50);
 		//player.setVolume(new_vol);
 	});
 
@@ -33,13 +50,18 @@ io.on('connection', (socket) => {
 
 	});
 
-	socket.on('current_req', () => {
-
+	socket.on('volume_req', () => {
+		let v = player.getVolume();
+		console.log('volume: ' + v);
+		socket.emit('volume', player.getVolume());
 	});	
 
-	socket.on('pause', () => {
-		 player.pause();
-		 console.log('pause');
+	socket.on('toggle_playing', () => {
+		let p = player.checkPlaying();
+		console.log('playing: ' + p);
+
+		if (p) player.mute();
+		else player.play();
 	});
 
 	socket.on('mute', () => {
@@ -49,6 +71,15 @@ io.on('connection', (socket) => {
 
 	socket.on('error', e => {
 		console.warn(e);
-	})
+	});
+
+	socket.on('add_alarm', d => {
+		d = Number(d);
+		console.log('New alarm added ' + new Date(d));
+		alarm_timestamps.push(d);
+
+		alarm_timestamps.sort((a, b) => b - a);
+		console.log(alarm_timestamps);
+	});
 
 });
--- public/js/wakeRadio.js
+++ public/js/wakeRadio.js
@@ -1,23 +1,31 @@
 var socket = io();
 
+$('#wake_date').val((new Date()).toISOString().substr(0, 10));
+
 $('#mute-btn').click(function(e) {
 	e.stopPropagation();
 	console.log('mute');
 	socket.emit('mute', null);
 });
 
-$('#pause-btn').click(function(e) {
+$('#toggle-btn').click(function(e) {
 	e.stopPropagation();
-	console.log('pause');
-	socket.emit('pause', null);
-});
-
-$('#play-btn').click(function(e) {
-	e.stopPropagation();
-	console.log('play');
-	socket.emit('play', null);
+	console.log('toggle');
+	socket.emit('toggle_playing', null);
 });
 
 $('#volume').change(function(e) {
+	console.log('set volume: ' + parseInt($('#volume').val()));
 	socket.emit('set_volume', parseInt($('#volume').val()));
 });
+
+$('#wake-btn').click(function(e) {
+	let timestamp = new Date($('#wake_date').val()).getTime() + parseInt($('#hours').val()) * 3600000 + parseInt($('#minutes').val()) * 60000;
+	socket.emit('add_alarm', timestamp);
+});
+
+/*
+socket.on('volume', v => {
+	console.log('volume: ' + v);
+});
+*/
